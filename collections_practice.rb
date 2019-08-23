a635aaf26cb5ef54685e74f02d20770062c3d1db
Daniel Lee <leedc@me.com>
2019-08-23 03:35:51 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,42 +19,42 @@ end
 #   array.sort { |index,destination_index| index[2] <=> destination_index[1] }
 # end
 
-def reverse_array(array)
-  array.reverse
-end
-
-def kesha_maker(array)
-  array.each { |char| char[2] = "$" }
-  array
-end
-
-def find_a(array)
-  array.select { |i| i.start_with?("a") }
-end
-
-# Methods for sum_array below
-# def sum_array(num) # Benchmark 0.00857
-#   sum = 0
-#   num.each { |i| sum += i }
-#   sum
+# def reverse_array(array)
+#   array.reverse
 # end
 #
-# def sum_array(num) # Benchmark 0.00806
-#   num.sum
+# def kesha_maker(array)
+#   array.each { |char| char[2] = "$" }
+#   array
 # end
-
-def sum_array(array) #Benchmark 0.00064
-  array.inject(:+)
-end
-
-def add_s(array)
-  # array.each_with_index.collect { |element, index| element.insert(-1,"s") if index != 1 }
-  array.each_with_index.collect do |element, index|
-    if index != 1
-      # "#{word}s"
-      element.insert(-1,"s")
-    else
-      element
-    end
-  end
-end
+#
+# def find_a(array)
+#   array.select { |i| i.start_with?("a") }
+# end
+#
+# # Methods for sum_array below
+# # def sum_array(num) # Benchmark 0.00857
+# #   sum = 0
+# #   num.each { |i| sum += i }
+# #   sum
+# # end
+# #
+# # def sum_array(num) # Benchmark 0.00806
+# #   num.sum
+# # end
+#
+# def sum_array(array) #Benchmark 0.00064
+#   array.inject(:+)
+# end
+#
+# def add_s(array)
+#   # array.each_with_index.collect { |element, index| element.insert(-1,"s") if index != 1 }
+#   array.each_with_index.collect do |element, index|
+#     if index != 1
+#       # "#{word}s"
+#       element.insert(-1,"s")
+#     else
+#       element
+#     end
+#   end
+# end
